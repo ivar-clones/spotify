6dc1937279defb161fa474e66bce6fd748bf1c18
Varun Gupta <varungupta2015135@gmail.com>
2024-08-11 13:12:01 +0530

    added compact and list view

--- src/components/internal/PlaylistBody/PlaylistBody.tsx
+++ src/components/internal/PlaylistBody/PlaylistBody.tsx
@@ -15,13 +15,22 @@ import {
   MoreHorizontalIcon,
   ListIcon,
   Clock3Icon,
+  AlignJustifyIcon,
 } from "lucide-react";
 import { useState } from "react";
 import { PlaylistBodyProps } from "./PlaylistBodyProps.interface";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const PlaylistBody = (props: PlaylistBodyProps) => {
   const { tracks } = props;
   const [isSticky, setIsSticky] = useState(false);
+  const [listView, setListView] = useState(true);
 
   const handleScroll = (e: any) => {
     if (e.target.scrollTop === 0) {
@@ -64,13 +73,48 @@ export const PlaylistBody = (props: PlaylistBodyProps) => {
           </Button>
         </div>
         <div className="flex flex-row items-center">
-          <Button
-            variant="ghost"
-            className="text-muted-foreground hover:bg-transparent hover:text-primary p-0"
-          >
-            List
-            <ListIcon className="ml-2" />
-          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                className="text-muted-foreground hover:bg-transparent hover:text-primary p-0"
+              >
+                List
+                <ListIcon className="ml-2" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40">
+              <DropdownMenuLabel>View as</DropdownMenuLabel>
+              <DropdownMenuCheckboxItem
+                checked={!listView}
+                onCheckedChange={() => {
+                  setListView(false);
+                }}
+              >
+                <Button
+                  variant="ghost"
+                  className="hover:text-primary p-0 h-fit"
+                >
+                  <AlignJustifyIcon className="mr-2" />
+                  Compact
+                </Button>
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={listView}
+                onCheckedChange={() => {
+                  setListView(true);
+                }}
+              >
+                <Button
+                  variant="ghost"
+                  className="hover:text-primary p-0 h-fit"
+                >
+                  <ListIcon className="mr-2" />
+                  List
+                </Button>
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
       <div className="mt-4">
@@ -80,7 +124,12 @@ export const PlaylistBody = (props: PlaylistBodyProps) => {
           >
             <TableRow className="hover:bg-transparent">
               <TableHead className="w-[5%] text-right">#</TableHead>
-              <TableHead className="w-[40%]">Title</TableHead>
+              <TableHead className={`${listView ? "w-[40%]" : "w-[30%]"}`}>
+                Title
+              </TableHead>
+              {!listView ? (
+                <TableHead className="w-[40%]">Artist</TableHead>
+              ) : undefined}
               <TableHead className="w-[40%]">Album</TableHead>
               <TableHead className="w-[5%]">
                 <Clock3Icon size={16} />
@@ -95,21 +144,30 @@ export const PlaylistBody = (props: PlaylistBodyProps) => {
                 </TableCell>
                 <TableCell className="text-muted-foreground">
                   <div className="flex flex-row gap-3 items-center">
-                    <Avatar className="rounded-lg h-10 w-10">
-                      <AvatarImage src={track.image.url} />
-                    </Avatar>
+                    {listView ? (
+                      <Avatar className="rounded-lg h-10 w-10">
+                        <AvatarImage src={track.image.url} />
+                      </Avatar>
+                    ) : undefined}
                     <div className="flex flex-col">
                       <div className="line-clamp-1 overflow-ellipsis text-primary">
                         {track.name}
                       </div>
-                      <div className="line-clamp-1 overflow-ellipsis">
-                        {track.artists}
-                      </div>
+                      {listView ? (
+                        <div className="line-clamp-1 overflow-ellipsis">
+                          {track.artists}
+                        </div>
+                      ) : undefined}
                     </div>
                   </div>
                 </TableCell>
+                {!listView ? (
+                  <TableCell className="text-muted-foreground line-clamp-1 overflow-ellipsis">
+                    {track.artists}
+                  </TableCell>
+                ) : undefined}
                 <TableCell className="text-muted-foreground">
-                  <div>{track.albumName}</div>
+                  {track.albumName}
                 </TableCell>
                 <TableCell className="text-muted-foreground rounded-tr-lg rounded-br-lg">
                   <div>{timeFormatterInMinSeconds(track.duration)}</div>
